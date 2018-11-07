2a5d7a2917f115beeb9b34bf054f224fcd74f767
titanabrian <titanabrian.05@gmail.com>
2018-11-08 05:38:19 +0700

    i push my repo

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+    <link rel="icon" type="image/svg+xml" href="./assets/doit.svg" sizes="192x192">
+    <title>Diponegoro IT Center | DoIT</title>
+    <!-- Bootstrap core CSS -->
+    <link href="template/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+  </head>
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-custom">
+      <div class="container">
+        <img id="navbar-logo" src="./assets/doit.svg" alt="">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" style="color:black !important"><i class="fas fa-angle-down"></i></span>
+        </button>
+        <div class="collapse navbar-collapse navbar-menu" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Purpose
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Division</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header with Background Image -->
+    <header class="business-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6" style="padding-top:8%">
+            <h4 class=" display-4 text-left text-white" style="width:30%">Diponegoro IT Center</h4>
+            <p>DoIT merupakan divisi pendukung dari Diponegoro Human Development Center (DHDC) yang bergerak dibidang Teknologi Informasi. Divisi ini bertugas pada penyediaan dan pengembangan produk berbasis IT berdasarkan kebutuhan dari pengguna</p>
+          </div>
+          <div class="col-md-6 text-right" style="padding-top:8%">
+            <img src="./assets/illustration-doit.svg" alt="doit illustration">
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row text-center" style="padding-top:4%">
+        <div class="col-md-12 col-lg-12 job-title text-center">
+          <h1>Program Diponegoro IT Center</h1>
+          <hr class="under-job-title"/>
+        </div>
+      </div>
+      <div class="row justify-content-center" style="margin-bottom:5%">
+        <div class="col-md-10 card-view">
+          <div class="card-title">
+            <img src="./assets/doit.svg" alt="DoIT icon">
+          </div>
+          <h5 class="card-sub-title">Web & Mobile App Development</h5>
+          <p>Tim DoIT mempunyai spesialisasi pada pemrograman website dan mobile yang akan membantu pembuatan aplikasi client</p>
+        </div>
+        <div class="col-md-10 card-view">
+          <div class="card-title">
+            <img src="./assets/doit.svg" alt="DoIT icon">
+          </div>
+          <h5 class="card-sub-title">IT Consulting</h5>
+          <p>Tim DoIT memberikan layanan IT Consulting dimana client akan mendapatkan solusi dari masalah - masalah bisnis yang dihadapi</p>
+        </div>
+        <div class="col-md-10 card-view">
+          <div class="card-title">
+            <img src="./assets/doit.svg" alt="DoIT icon">
+          </div>
+          <h5 class="card-sub-title">Programming Class</h5>
+          <p>Tim DoIT memberikan layanan pelatihan dalam pembuatan aplikasi web ataupun mobile</p>
+        </div>
+      </div>
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <div class="container-fluid">
+      <div class="row bg-super-light text-center">
+        <div class="col-md-6 col-lg-6 col-xs-6 text-left footer-logo-container">
+          <img class="footer-logo" src="./assets/logo-uccp.svg" alt="logouccp">
+          <h3>PT Undip Citra Ciptaprima</h3>
+          <p>Jalan Prof Sudarto No. 455 Gendung UCC Universitas Diponegoro,
+            Tembalang, Semarang, Jawa Tengah 50275
+            (024)6778965
+          </p>
+        </div>
+        <div class="col-md-6 text-left">
+          <ul class="horizontal-list">
+            <li><a href="#">Diponizer</a></li>
+            <li><a href="#">DoIT</a></li>
+            <li><a href="#">Direct</a></li>
+            <li><a href="#">Decent</a></li>
+          </ul>
+          <ul class="horizontal-list light-list" style="padding-top:0% !important">
+            <li><a href="#">About Out Company</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+          <!-- <ul class="horizontal-list" style="padding-top:0% !important">
+            <div><img src="./assets/doit.svg"/></div>
+            <div><img src="./assets/doit.svg"/></div>
+            <div><img src="./assets/doit.svg"/></div>
+          </ul> -->
+        </div>
+      </div>
+    </div>
+    <footer class="py-5 bg-light">
+      <div class="container">
+        <p class="m-0 text-center text-dark">PT UCCP All Rights Reserved 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="template/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="template/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+      $('#navbar-logo').click(function(){
+        window.location.replace('./');
+      })
+    </script>
+  </body>
+
+</html>
